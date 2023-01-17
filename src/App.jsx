0a647b7e28b1989a87bda89dfad654fccb9b7868
TommyOliveir <laridatommy@gmail.com>
2023-01-17 17:36:13 +0100

    f

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import { Route, Routes } from "react-router-dom"
-import About from './pages/about'
 import Home from './pages/Home'
 
 function App() {
