4c3503ab74ae0661d6494259dce1444fce931de9
TommyOliveir <laridatommy@gmail.com>
2023-01-23 14:21:46 +0100

    luch

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,20 +8,20 @@ function About() {
         <h2>ABOUT ME</h2>
         <p>I'm a frontend developer based out of London, UK. I love building apps that solve real-world problems, and that are delightful to use. My specialities include TypeScript, React JS, Tailwind CSS, and Styled Components.</p>
       </div>
-      <div className="about-image">
+      <div id='about-image' className="about-image">
 
       </div>
       <div className="get-to-know-me about-grid-item">
         <h3>Get to know me!</h3>
         <p>My background is in teaching and marketing. I have a bachelors degree in English from Kings College. When I'm not coding, I take care of my five adorable cats.</p>
-       
-      
-      </div>
-      <div className="video">
-          <iframe className="video-item" src="https://www.youtube.com/embed/tgbNymZ7vqY?controls=0">
-          </iframe>
 
-        </div>
+
+      </div>
+      <div className="video about-grid-item">
+        <iframe className="video-item" src="https://www.youtube.com/embed/tgbNymZ7vqY?controls=0">
+        </iframe>
+
+      </div>
       <div className="skills about-grid-item">
         <h3>My Skills</h3>
         <ul>
@@ -33,10 +33,10 @@ function About() {
           <li>Git</li>
           <li>Git</li>
         </ul>
-       
-      
+
+
       </div>
-     
+
     </div>
   )
 }
