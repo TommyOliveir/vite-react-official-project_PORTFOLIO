6b0bada7c078ef8c715e0d6198ce1e2eabd7729c
TommyOliveir <laridatommy@gmail.com>
2023-01-27 17:30:56 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 function Footer() {
   return (
     <>
-      <div className="social-footer">footer</div>
-      <div className="div">
-      <p>© Copyright 2023 - TommyTheDevRanger </p>
+      <div className="social-footer">footer
+        <div>
+          <p>© Copyright 2023 - TommyTheDevRanger </p>
+        </div>
       </div>
-     
+
+
     </>
 
   )
