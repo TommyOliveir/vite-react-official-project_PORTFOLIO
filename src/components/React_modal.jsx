e521d97f7058e2d4adfc68e43492dc9f57d6e1cc
TommyOliveir <laridatommy@gmail.com>
2023-01-31 17:15:49 +0100

    modal

---
+++ src/components/React_modal.jsx
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+import ReactDOM from 'react-dom'
+import ModalVideo from 'react-modal-video'
+
+
+function React_modal() {
+    const [isOpen, setOpen] = useState(false)
+
+	return (
+		<React.Fragment>
+			<ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="0UJYYkK4d8s" onClose={() => setOpen(false)} />
+
+			<button className="btn-primary" onClick={()=> setOpen(true)}>VIEW DEMO</button>
+          
+		</React.Fragment>
+	)
+}
+
+export default React_modal
